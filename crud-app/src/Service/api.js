b4d94241e8bec8b2f4e037e7b301f76c140d87f3
Nirmal Kumar N <NirmalKumar.n@avasoft.com>
2022-03-20 12:02:32 +0530

    final-commit

--- crud-app/src/Service/api.js
+++ crud-app/src/Service/api.js
@@ -13,7 +13,7 @@ export const addUser = async (user) => {
 }
 
 export const deleteUser = async (id) => {
-    return await axios.delete(`${usersUrl}/${id}`);
+    return await axios.delete('https://nodejs-nirmal.azurewebsites.net/api/CreateUsers?code=eaa5PLg9j7Z88dUJKPLrIn3rQxUkzcUByQ9J5VLAHen3NATJBOOYIA==', id);
 }
 
 export const editUser = async (id, user) => {
